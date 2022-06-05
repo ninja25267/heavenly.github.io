cb9c300e503d579e031729ab546b4e138266da40
ninja25267 <52323378+ninja25267@users.noreply.github.com>
2022-06-05 22:51:44 +0800

    Add files via upload

--- js/data_action.js
+++ js/data_action.js
@@ -1,16 +1,40 @@
 data_action = [
     {
         title: "Attack",
-        icon: "crossed-swords",
+        icon: "spinning-sword",
         subtitle: "Melee or ranged attack",
         description: "Perform a melee or ranged attack with your weapon",
         reference: "PHB, pgs. 192,194-195.",
         bullets: [
             "Certain features, such as the <i>Extra Attack</i> feature of the fighter, allow you to make more than one attack with this action. Each of these attacks is a separate roll and may target different creatures. You may move in between these attacks.",
-            "When you attack with a light melee weapon, you can use a bonus action to attack with your other hand (see the <i>Offhand attack</i> bonus action).",
+            "When you attack with a melee weapon you can engage in <i>Two-Weapon Fighting</i>.",
             "You may replace one of your melee attacks with a <i>Grapple</i> or a <i>Shove</i>.",
             "Some conditions give advantage on the attack: attacks against blinded, paralyzed, petrified, restrained, stunned, or unconscious targets; melee attacks against prone targets; attacks by invisible or hidden attackers.",
             "Some conditions give disadvantage on the attack: attacks against invisible or hidden targets; ranged attacks against prone targets; attacks by blinded, frightened, poisoned, or restrained attackers."
+        ]
+    },
+    {
+        title: "Two-Weapon Fighting",
+        icon: "crossed-swords",
+        subtitle: "Make an offhand attack",
+        description: "Perform an offhand attack with your weapon",
+        reference: "WCX, pg. 87.",
+        bullets: [
+            "When you attack with a melee weapon or make a special melee attack using one hand, you can make an additional attack with a light melee weapon you're holding in the other.",
+            "You don't add your ability modifier to the damage of this attack unless it is negative.",
+            "You cannot use this and <i>Versatile Fighting</i> on the same turn."
+        ]
+    },
+    {
+        title: "Versatile Fighting",
+        icon: "winged-sword",
+        subtitle: "Make an versatile weapon attack",
+        description: "Perform an attack with a versatile weapon",
+        reference: "WCX, pg. 87.",
+        bullets: [
+            "When you make a special melee attack using one hand, you can make an additional attack with a versatile melee weapon you're holding in the other.",
+            "You make this attack with one hand on the weapon, even if you have a free hand.",
+            "You cannot use this and <i>Two-Weapon Fighting</i> on the same turn."
         ]
     },
     {
@@ -18,12 +42,15 @@ data_action = [
         icon: "grab",
         subtitle: "Special melee attack",
         description: "Attempt to grab a creature or wrestle with it",
-        reference: "PHB, pg. 195.",
+        reference: "PHB, pg. 195, WCX, pg. 87.",
         bullets: [
             "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "When you make a special melee attack using one hand, you can engage in <i>Versatile Fighting</i>.",
             "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
             "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
-            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
+            "If you succeed, you subject the target to the grappled condition.",
+            "When you move, you can drag or carry the grappled creature with you, but your speed is halved, unless the creature is two or more sizes smaller than you.",
+            "You can use the <i>Shove</i> action to move a creature that you have grappled to another space within your reach without breaking the grapple."
         ]
     },
     {
@@ -34,11 +61,29 @@ data_action = [
         reference: "PHB, pg. 195.",
         bullets: [
             "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "When you make a special melee attack using one hand, you can engage in <i>Versatile Fighting</i>.",
             "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
             "You make a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
             "If you win the contest, you either knock the target prone or push it 5 feet away from you.",
             "Alternatively you can push the target to the side but you must make the Strength (Athletics) check at disadvantage.",
             "If you win the contest, you move the target to a different space within your reach."
+        ]
+    },
+    {
+        title: "Toss",
+        icon: "impact-point",
+        subtitle: "Toss a creature",
+        description: "Attempt to toss a creature you're grappling",
+        reference: "WCX, pg. 87.",
+        bullets: [
+            "While you are grappling a creature you can use the <i>Attack</i> action to make a special melee attack to hurl the grappled creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "When you make a special melee attack using one hand, you can engage in <i>Versatile Fighting</i>.",
+            "To toss a creature, you must make a Strength (Athletics) check against a DC determined by your size and the target creature's size.",
+            "To toss a tiny creature is a DC of 5, small is a DC of 11, medium is a DC of 17, large is a DC of 23, huge is a DC of 29, gargantuan is a DC of 35.",
+            "The DC increases by 5 if you're small and 10 if you're tiny, and the DC decreases by 5 if you're large, 10 if you're huge, and 15 if you're gargantuan.",
+            "If you succeed, you throw the creature a distance yp to 5 times your Strength modifier + 5 in feet.",
+            "When it lands, the thrown creature takes bldugeoning damage for every 10 feet it traveled while thrown, as if it had taken falling damage from a height of the full distance.",
+            "The tossed creature lands prone unless it succeeds on a Dexterity (Acrobatics) check with a DC equal to the Strength (Athletics) check used to toss or uses a feature or spell to land safely."
         ]
     },
     {
@@ -48,7 +93,6 @@ data_action = [
         description: "Cast a spell with a casting time of 1 action",
         reference: "PHB, pg. 192.",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
             "The target of a spell must be within the spell's range. To target something, you must have a clear path to it, so it can't be behind total cover.",
             "Spells with material components do not consume the material unless explicitly stated. Unless the cost of a material is given, you can assume that the cost is negligible and the material is simply available in a component pouch.",
             "Some spells require you to maintain concentration in order to keep their magic active. If you lose concentration, such a spell ends. You lose concentration on a spell if you cast another spell that requires concentration or when you are incapacitated. Each time you take damage, you must make a Constitution saving throw to maintain your concentration. The DC equals 10 or half the damage you take, whichever number is higher."
@@ -103,7 +147,7 @@ data_action = [
         reference: "PHB, pg. 192.",
         bullets: [
             "The target gains advantage on the next ability check it makes to perform the task you are helping with.",
-            "Alternatively, the target gains advantage on the next attack roll against against a creature within 5 feet of you.",
+            "Alternatively, the target gains advantage on its next attack roll.",
             "The advantage lasts until the start of your next turn."
         ]
     },
@@ -150,7 +194,9 @@ data_action = [
         description: "Devote your attention to finding something",
         reference: "PHB, pg. 193.",
         bullets: [
-            "Depending on the nature of your search, the DM might have you make a Wisdom (Perception) check or an Intelligence (Investigation) check."
+            "Depending on the nature of your search, the DM might have you make a Wisdom (Perception) check or an Intelligence (Investigation) check.",
+            "If you are searching a creature, the DM might have you roll an Intelligence (Intel) check or another appropriate skill.",
+            "Successfully using search on a creature allows you to figure out it's features or stats."
         ]
     },
     {
